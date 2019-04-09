392182d99cfd237634b9cbbe75085d038deaded8
Edgar Velazquez <edgarvl@gmail.com>
2019-04-09 00:03:16 -0500

    Primer commit

---
+++ spring5webapp/src/main/java/com/velmen/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.velmen.spring5webapp.repositories;
+
+import com.velmen.spring5webapp.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository  extends CrudRepository<Author,Long> {
+}
